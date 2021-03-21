a262db4fe971fee48dbbdd81dede7cb6dce4a00c
Felipe Ranzoni Borges <felipe.ranzoni@hotmail.com>
2021-03-21 12:34:58 -0300

    Projeto do curso

---
+++ Assets/Game/Scripts/GameController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private bool isGameRunning;
+    private int score;
+    private int currentLevelIndex;
+
+    public ObjectGenerator generator;
+    public GameConfiguration config;
+    public TextMeshProUGUI scoreLabel;
+    public GameUI gameStartUI;
+    public GameUI gameOverUI;
+    public Player player;
+    public LevelConfiguration[] levels;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGameRunning = false;
+
+        gameStartUI.Show();
+        config.speed = 0f;
+    }
+
+    void Update()
+    {
+        scoreLabel.text = score.ToString("000000000.##");
+        if (!isGameRunning) return;
+        score++;
+        CheckLevelUpdate();
+    }
+
+    public void GameStart()
+    {
+        currentLevelIndex = 0;
+        SetCurrentLevelConfiguration();
+
+        isGameRunning = true;
+
+        generator.GenerateObstacles();
+        score = 0;
+        gameStartUI.Hide();
+        player.SetActive();
+    }
+
+    private void CheckLevelUpdate()
+    {
+        if (currentLevelIndex >= levels.Length - 1) return;
+        if (score < levels[currentLevelIndex + 1].minScore) return;
+        currentLevelIndex++;
+
+        SetCurrentLevelConfiguration();
+    }
+
+    public void SetCurrentLevelConfiguration()
+    {    
+        LevelConfiguration level = levels[currentLevelIndex];
+        config.speed = level.speed;
+        config.minRangeObstacleGenerator = level.minRangeObstacleGenerator;
+        config.maxRangeObstacleGenerator = level.maxRangeObstacleGenerator;
+    }
+
+    public void GameOver()
+    {
+        isGameRunning = false;
+
+        config.speed = 0f;
+        generator.StopGenerator();
+        gameOverUI.Show();
+    }
+
+    public void RestartGame()
+    {
+        gameOverUI.Hide();
+        generator.ResetGenerator();
+        GameStart();
+    }
+}
---
+++ Assets/Game/Scripts/Obstacle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    private Rigidbody2D objRB;
+
+    public GameConfiguration config;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objRB = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        objRB.velocity = new Vector2(-config.speed, 0f);
+    }
+}
